47c0f18a47927463c7300f82ccdf5dbd2bdf8bd2
Que Hua <huathuque@gmail.com>
2022-03-16 15:25:18 +0700

    function, lambda function

---
+++ calculation.dart
@@ -0,0 +1,8 @@
+int tinhTong(int a,int b){
+
+  return a + b;
+}
+
+// thay vì viết như trên viết thành
+// int tinhTong(int a, int b) => a + b;
+// => đây gọi là lambda function
--- main.dart
+++ main.dart
@@ -1,12 +1,60 @@
+import 'message.dart';
+import 'calculation.dart';
+
 void main(){
-  String a = 'a';
-  String b = 'b';
+  // String a = 'a';
+  // String b = 'b';
+  //
+  // StringBuffer buffer = StringBuffer();
+  //
+  // buffer.write(a);
+  // buffer.write(b);
+  // buffer.write('noiduoi');
+  //
+  // print(buffer.toString());
 
-  StringBuffer buffer = StringBuffer();
 
-  buffer.write(a);
-  buffer.write(b);
-  buffer.write('noiduoi');
+  // toán tử
 
-  print(buffer.toString());
+  // var a = 5;
+  // var b = a++;
+  //
+  // print(a);
+  // print(b);
+
+  // var a ;
+  //
+  // a ??= 10;
+
+  // var a = 123;
+  //
+  // if (a is int){
+  //   print('a co kieu du lieu la int');
+  // }
+
+  // câu điều kiện
+  // var text = '';
+  //
+  // if (text.isEmpty){
+  //   print('chuỗi rỗng');
+  // } else {
+  //   print('có giá trị');
+  // }
+
+ // var month = 4;
+ //
+ // switch (month) {
+ //   case 4 :
+ //     print('Tháng 4');
+ //         break;
+ // }
+
+  // var a = 5;
+  //
+  // assert(a != null, "A là null");
+
+  printMessage('Thông báo');
+
+  var result = tinhTong(4, 5);
+  print(result.toString());
 }
